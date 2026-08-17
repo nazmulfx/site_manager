84634e4a3e5fbcac9e50e747c5611587d9cf3937
Nazmul Hossain <nazmulhossain664@gmail.com>
2026-08-17 17:00:42 +0600

    feat: add NodeVersion, PythonVersion, and SiteManagerSettings Doctype definitions

---
+++ site_manager/site_manager/doctype/node_version/node_version.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Nazmul Hossain and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Node Version", {
+// 	refresh(frm) {
+
+// 	},
+// });
